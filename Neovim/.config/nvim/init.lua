00f608abc964bf3dd2e3877c73617758a1ab5298
barnz <hi@barnz.dev>
2023-11-29 19:42:29 +0000

    neovim: set up format on save

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -491,9 +491,16 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  --vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --  vim.lsp.buf.format()
+  --end, { desc = 'Format current buffer with LSP' })
+
+  -- set up autocommand to use lsp to format file on save
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    callback = function ()
+      vim.lsp.buf.format();
+    end
+  })
 end
 
 -- document existing key chains
