1e34a788664c107da37c8876c339542160957a90
barnz <hi@barnz.dev>
2025-04-12 11:38:55 +0100

    i3: update mouse script for MX Ergo

--- i3/.config/i3/mx_ergo.sh
+++ i3/.config/i3/mx_ergo.sh
@@ -5,7 +5,9 @@ check=$(xinput | grep "$mouse_name")
 
 if [[ -n "$check" ]]; then
   # grab mouse id from xinput
-  mouse_id=$(xinput | grep "$mouse_name" | sed 's/^.*id=\([0-9]*\)[ \t].*$/\1/')
+  # MX Ergo has a virtual keyboard of the same name
+  # so only grab the first id as mice are listed first
+  mouse_id=$(xinput | grep "$mouse_name" | sed 's/^.*id=\([0-9]*\)[ \t].*$/\1/' | head -n 1)
 
   # disable mouse acceleration
   xinput set-prop "$mouse_id" "libinput Accel Profile Enabled" 0, 1
